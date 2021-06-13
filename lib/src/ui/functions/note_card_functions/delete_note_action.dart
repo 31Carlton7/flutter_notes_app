7e4998c6a173028e2ee6a4531ea1608fa6b01772
31Carlton7 <carltonaikins7@gmail.com>
2021-06-13 19:07:49 -0400

    Restyle delete action

--- lib/src/ui/functions/note_card_functions/delete_note_action.dart
+++ lib/src/ui/functions/note_card_functions/delete_note_action.dart
@@ -1,30 +1,37 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:notes_app/src/services/repositories/note_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes_app/src/models/note.dart';
+import 'package:notes_app/src/ui/providers/note_provider.dart';
 
-class DeleteNoteAction extends StatelessWidget {
-  const DeleteNoteAction(this.repo, this.note);
-  final NoteRepository repo;
+class DeleteNoteAction extends ConsumerWidget {
+  const DeleteNoteAction(this.note, this.setState);
+
   final Note note;
+  final void Function(void Function()) setState;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
     return Container(
       margin: EdgeInsets.only(top: 5, bottom: 5, left: 5),
-      child: Material(
-        color: Theme.of(context).colorScheme.onError,
-        shape: SquircleBorder(
-          radius: BorderRadius.circular(35),
-        ),
-        child: SlideAction(
-          child: IconlyIcon(
-            IconlyBold.Delete,
-            size: 27,
-            color: Theme.of(context).colorScheme.error,
+      child: SlideAction(
+        decoration: ShapeDecoration(
+          color: Theme.of(context).colorScheme.onError,
+          shape: SquircleBorder(
+            radius: BorderRadius.circular(35),
           ),
-          onTap: () => repo.removeNote(note),
         ),
+        child: IconlyIcon(
+          IconlyBold.Delete,
+          size: 27,
+          color: Theme.of(context).colorScheme.error,
+        ),
+        onTap: () {
+          setState(() {
+            watch(noteProvider.notifier).removeNote(note);
+          });
+        },
       ),
     );
   }
