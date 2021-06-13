6d1240869128453a8bd456176e77aac0b2bec302
31Carlton7 <carltonaikins7@gmail.com>
2021-06-13 19:10:34 -0400

    Add tags to note creation functionality

--- lib/src/ui/views/note_creation_view.dart
+++ lib/src/ui/views/note_creation_view.dart
@@ -1,3 +1,4 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -17,35 +18,32 @@ class _NoteCreationViewState extends State<NoteCreationView> {
   /// TextEditingControllers
   final _titleController = TextEditingController();
   final _contentController = TextEditingController();
-  var _passwordController = TextEditingController();
 
   /// Focus Nodes
   final _contentFocus = FocusNode();
   final _titleFocus = FocusNode();
-  final _passwordFocus = FocusNode();
 
   /// Tags
-  final _tags = [];
-
-  void newNoteFunction() {
-    if (widget.note!.id != null) {
-      _titleController.text = widget.note!.title ?? '';
-      _contentController.text = widget.note!.content ?? '';
-      _passwordController = TextEditingController(text: widget.note!.password);
-    }
-  }
+  List<Tag>? _tags = [];
 
   @override
   void initState() {
     super.initState();
-    newNoteFunction();
+    _editNoteFunction();
+  }
+
+  void _editNoteFunction() {
+    if (widget.note?.id != null) {
+      _titleController.text = widget.note?.title ?? '';
+      _contentController.text = widget.note?.content ?? '';
+      _tags = widget.note?.tags;
+    } else if (widget.note?.tags != null) {
+      _tags = widget.note?.tags;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    /// Requests focus on [TextField] if id is null
-    FocusScope.of(context).requestFocus(_titleFocus);
-
     return CantonScaffold(
       body: _content(context),
     );
@@ -68,29 +66,16 @@ class _NoteCreationViewState extends State<NoteCreationView> {
   Widget _header(BuildContext context, ScopedReader watch) {
     return ViewHeaderTwo(
       title: NoteRepository.dateTimeString(
-        widget.note!.creationDate ?? DateTime.now(),
-      ).substring(6),
-      backButton: true,
-      backButtonFunction: () => _addNoteFunction(watch),
-      buttonTwo: CantonHeaderButton(
-        backgroundColor: CantonColors.transparent,
-        icon: Icon(
-          FeatherIcons.moreVertical,
-          color: Theme.of(context).primaryColor,
-          size: 27.0,
-        ),
-        onPressed: () {
-          _noteConfigurations(_passwordController);
-        },
+        widget.note!.lastEditDate ?? DateTime.now(),
       ),
+      backButton: true,
+      backButtonFunction: () => _completeNoteFunction(watch),
     );
   }
 
   Widget _body(BuildContext context) {
     return Consumer(
       builder: (context, watch, child) {
-        final repo = watch(noteProvider.notifier);
-
         return Expanded(
           child: Column(
             children: [
@@ -111,7 +96,15 @@ class _NoteCreationViewState extends State<NoteCreationView> {
       controller: _titleController,
       maxLines: null,
       scrollController: new ScrollController(),
-      onChanged: (_) {},
+      onChanged: (title) {
+        setState(() {
+          context.read(noteProvider.notifier).updateNote(
+                note: widget.note!,
+                title: title,
+                lastEditDate: DateTime.now(),
+              );
+        });
+      },
       style: Theme.of(context).textTheme.headline3,
       decoration: InputDecoration(
         hintText: 'Title',
@@ -131,12 +124,18 @@ class _NoteCreationViewState extends State<NoteCreationView> {
   }
 
   Widget _tagTextField(BuildContext context) {
+    final _initTags = <String>[];
+
+    for (final tag in _tags!) {
+      _initTags.add(tag.name);
+    }
+
     return CantonTagTextInput(
-      initialTags: [],
+      initialTags: _initTags,
       textFieldStyler: TagTextInputStyler(
         cursorColor: Theme.of(context).primaryColor,
         hintText: 'Tags',
-        textFieldFilledColor: Theme.of(context).colorScheme.onSecondary,
+        textFieldFilledColor: Theme.of(context).colorScheme.secondary,
         textFieldFilled: true,
         textFieldEnabledBorder: SquircleInputBorder(
           radius: BorderRadius.all(Radius.circular(35)),
@@ -177,9 +176,19 @@ class _NoteCreationViewState extends State<NoteCreationView> {
             .bodyText1!
             .copyWith(color: CantonColors.white),
       ),
-      onDelete: (_) {},
+      onDelete: (name) {
+        _tags?.remove(Tag(name: name));
+        setState(() {
+          context.read(noteProvider.notifier).updateNote(
+              note: widget.note!, tags: _tags, lastEditDate: DateTime.now());
+        });
+      },
       onTag: (name) {
-        _tags.add(name);
+        _tags?.add(Tag(name: name));
+        setState(() {
+          context.read(noteProvider.notifier).updateNote(
+              note: widget.note!, tags: _tags, lastEditDate: DateTime.now());
+        });
       },
     );
   }
@@ -191,7 +200,15 @@ class _NoteCreationViewState extends State<NoteCreationView> {
       controller: _contentController,
       maxLines: null,
       scrollController: new ScrollController(),
-      onChanged: (_) {},
+      onChanged: (content) {
+        setState(() {
+          context.read(noteProvider.notifier).updateNote(
+                note: widget.note!,
+                content: content,
+                lastEditDate: DateTime.now(),
+              );
+        });
+      },
       style: Theme.of(context).textTheme.headline6,
       decoration: InputDecoration(
         fillColor: CantonColors.transparent,
@@ -206,7 +223,7 @@ class _NoteCreationViewState extends State<NoteCreationView> {
     );
   }
 
-  void _addNoteFunction(ScopedReader watch) {
+  void _completeNoteFunction(ScopedReader watch) {
     final repo = watch(noteProvider.notifier);
 
     if (!['', null].contains(_contentController.text) ||
@@ -214,24 +231,13 @@ class _NoteCreationViewState extends State<NoteCreationView> {
       // Creates a new note if the controllers
       // aren't empty. If empty, it will act as a regular
       // back button.
-      if (widget.note!.id != null) {
-        repo.updateNote(
-          note: widget.note!,
-          title: _titleController.text,
-          content: _contentController.text,
-          pinned: widget.note!.pinned ?? false,
-          locked: widget.note!.locked ?? false,
-          password: !['', null].contains(_passwordController.text)
-              ? _passwordController.text
-              : '',
-          lastEditDate: widget.note!.lastEditDate!,
-        );
-      } else {
+      if (widget.note!.id == null) {
         repo.addNote(
           Note(
             id: UniqueKey().toString(),
             title: _titleController.text,
             content: _contentController.text,
+            tags: _tags,
             pinned: widget.note!.pinned ?? false,
             locked: widget.note!.locked ?? false,
             password: widget.note!.password ?? '',
@@ -243,201 +249,5 @@ class _NoteCreationViewState extends State<NoteCreationView> {
     } else {
       if (widget.note!.id != null) repo.removeNote(widget.note!);
     }
-  }
-
-  Future<dynamic> _noteConfigurations(
-    TextEditingController _passwordController,
-  ) {
-    return showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      backgroundColor: CantonColors.bgPrimary,
-      elevation: 0,
-      useRootNavigator: true,
-      shape: SquircleBorder(radius: BorderRadius.circular(55)),
-      builder: (_) {
-        return Consumer(
-          builder: (context, watch, child) {
-            bool _pWordIsEmpty = false;
-            return StatefulBuilder(
-              builder: (context, setState) => Padding(
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 27),
-                child: FractionallySizedBox(
-                  heightFactor: 0.95,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Container(
-                        height: 5,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          color: cantonGrey[300],
-                        ),
-                      ),
-                      SizedBox(height: 15),
-                      Text(
-                        'Configure your Note',
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      const SizedBox(height: 30),
-                      Row(
-                        children: [
-                          pinNoteButton(widget.note!, setState),
-                          SizedBox(width: 7),
-                          lockNoteButton(_pWordIsEmpty),
-                        ],
-                      ),
-                      const SizedBox(height: 7),
-                      Row(
-                        children: [
-                          CantonPrimaryButton(
-                            buttonText: 'Cancel',
-                            containerColor: CantonColors.bgDanger,
-                            textColor: CantonColors.textDanger,
-                            containerWidth:
-                                MediaQuery.of(context).size.width / 2 - 54,
-                            onPressed: () => Navigator.pop(context),
-                          ),
-                          Spacer(),
-                          CantonPrimaryButton(
-                            buttonText: 'Save',
-                            containerColor: CantonColors.bgInverse,
-                            textColor: CantonColors.bgPrimary,
-                            containerWidth:
-                                MediaQuery.of(context).size.width / 2 - 54,
-                            onPressed: () {
-                              widget.note!.password = _passwordController.text;
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  Widget pinNoteButton(Note note, void Function(void Function()) setState) {
-    Widget pinnedIcon = [null, false].contains(note.pinned)
-        ? Icon(
-            CupertinoIcons.pin_fill,
-            color: CantonColors.yellow,
-          )
-        : Icon(
-            CupertinoIcons.pin_slash_fill,
-            color: CantonColors.yellow,
-          );
-    return CantonHeaderButton(
-        icon: pinnedIcon,
-        backgroundColor: CantonColors.gray[200],
-        onPressed: () {
-          setState(() {
-            note.pinned = !note.pinned!;
-          });
-        });
-  }
-
-  Widget lockNoteButton(bool _pWordIsEmpty) {
-    Widget lockedIcon = [null, false].contains(widget.note!.locked)
-        ? Icon(
-            CupertinoIcons.lock_fill,
-            color: CantonColors.purple,
-          )
-        : Icon(
-            CupertinoIcons.lock_slash_fill,
-            color: CantonColors.purple,
-          );
-    return CantonHeaderButton(
-      icon: lockedIcon,
-      backgroundColor: CantonColors.gray[200],
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (_) => AlertDialog(
-            shape: SquircleBorder(radius: BorderRadius.circular(55)),
-            elevation: 0,
-            title: Text(
-              'Configure Lock',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            content: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Create a password for your note',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6!
-                      .copyWith(color: CantonColors.textSecondary),
-                ),
-                SizedBox(height: 7),
-                CantonTextInput(
-                  obscureText: true,
-                  isTextFormField: true,
-                  controller: _passwordController,
-                  focusNode: _passwordFocus,
-                  textInputType: TextInputType.number,
-                ),
-                SizedBox(height: 10),
-                _pWordIsEmpty
-                    ? Text(
-                        'Please Enter a password',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(color: CantonColors.textDanger),
-                      )
-                    : Container(),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    CantonPrimaryButton(
-                      buttonText: 'Cancel',
-                      containerColor: CantonColors.bgDanger,
-                      textColor: CantonColors.textDanger,
-                      containerWidth:
-                          MediaQuery.of(context).size.width / 2 - 70,
-                      onPressed: () {
-                        _passwordController = TextEditingController();
-                        Navigator.pop(context);
-                      },
-                    ),
-                    Spacer(),
-                    CantonPrimaryButton(
-                      buttonText: 'Save',
-                      containerColor: CantonColors.bgInverse,
-                      textColor: CantonColors.bgPrimary,
-                      containerWidth:
-                          MediaQuery.of(context).size.width / 2 - 70,
-                      onPressed: () {
-                        setState(() {
-                          _passwordController.text.isNotEmpty
-                              ? widget.note!.locked = true
-                              : widget.note!.locked = false;
-                        });
-                        Navigator.pop(context);
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 30),
-                Text(
-                  'To remove the password Lock, simply delete the text in the text field.',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
   }
 }
