c789bfa8194c1da9290b3f75a6e6ad4fd51f4b52
31Carlton7 <carltonaikins7@gmail.com>
2021-06-13 19:08:14 -0400

    Update provider

--- lib/src/ui/providers/note_provider.dart
+++ lib/src/ui/providers/note_provider.dart
@@ -2,5 +2,6 @@ import 'package:notes_app/src/services/repositories/note_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes_app/src/models/note.dart';
 
-final noteProvider = StateNotifierProvider<NoteRepository, List<Note>>(
-    (ref) => NoteRepository());
+final noteProvider = StateNotifierProvider<NoteRepository, List<Note>>((ref) {
+  return NoteRepository();
+});
