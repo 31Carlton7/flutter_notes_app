f221e5cfc77fa8b10de18d12e11c51892f488205
31Carlton7 <carltonaikins7@gmail.com>
2021-06-13 19:07:57 -0400

    Restyle pin action

--- lib/src/ui/functions/note_card_functions/pin_note_action.dart
+++ lib/src/ui/functions/note_card_functions/pin_note_action.dart
@@ -1,35 +1,41 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:notes_app/src/services/repositories/note_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes_app/src/models/note.dart';
+import 'package:notes_app/src/ui/providers/note_provider.dart';
 
-class PinNoteAction extends StatelessWidget {
+class PinNoteAction extends ConsumerWidget {
+  const PinNoteAction(this.note, this.setState);
+
   final Note note;
-  final NoteRepository repo;
+  final void Function(void Function()) setState;
 
-  const PinNoteAction(this.repo, this.note);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
     return Container(
-      margin: EdgeInsets.only(top: 5, bottom: 5),
-      child: Material(
-        color: CantonColors.yellow,
-        shape: SquircleBorder(
-          radius: BorderRadius.circular(35),
-        ),
-        child: SlideAction(
-          child: Icon(
-            note.pinned!
-                ? CupertinoIcons.pin_slash_fill
-                : CupertinoIcons.pin_fill,
-            size: 27,
-            color: CantonColors.gray[100],
+      margin: EdgeInsets.only(top: 5, bottom: 5, right: 5),
+      child: SlideAction(
+        decoration: ShapeDecoration(
+          color: Theme.of(context).colorScheme.onSurface,
+          shape: SquircleBorder(
+            radius: BorderRadius.circular(35),
           ),
-          onTap: () {
-            repo.updateNote(note: note, pinned: !note.pinned!);
-          },
         ),
+        child: Icon(
+          note.pinned!
+              ? CupertinoIcons.pin_slash_fill
+              : CupertinoIcons.pin_fill,
+          size: 27,
+          color: Theme.of(context).colorScheme.surface,
+        ),
+        onTap: () {
+          setState(() {
+            watch(noteProvider.notifier)
+                .updateNote(note: note, pinned: !note.pinned!);
+          });
+        },
       ),
     );
   }
